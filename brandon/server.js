a3f22e49b4fea50a1f240970c60cbad85483f638
BRANDON PARKER <Brandon@dkparker.com>
2016-04-12 12:26:33 -0700

    added gulpfile and fixed bugs when running the gulp command

--- brandon/server.js
+++ brandon/server.js
@@ -5,7 +5,7 @@ const server = net.createServer((socket) => {
   socket.pipe(process.stdout);
   const text = fs.createWriteStream(__dirname + '/logs/log_' + Date.now() + '.txt');
   socket.pipe(text);
-  socket.write("HTTP/1.1 200 OK");
+  socket.write('HTTP/1.1 200 OK');
   socket.on('timeout', () => {
     socket.end();
   });
@@ -14,7 +14,7 @@ const server = net.createServer((socket) => {
 });
 
 server.listen(3000, () => {
-  process.stdout.write('Server created on route 3000\n')
+  process.stdout.write('Server created on route 3000\n');
 });
 
 exports.server = server;
